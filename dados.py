cf40b979596e827642cb4d70eec083a301ba8cf9
Vinicius Nascimento <viniciusn463@gmail.com>
2026-02-14 21:23:52 -0300

    atualizações: media e loop

--- dados.py
+++ dados.py
@@ -1,37 +1,57 @@
-print("--------- Analisador de Notas ---------")
-print(" ")
-# nome_aluno = (input("Informe o nome do aluno: "))
-# matricula_aluno = (input("Informe a matrícula do aluno: "))
-# n1 = (input("Informe a primeira nota do aluno: "))
-# n2 = (input("Informe a segunda nota do aluno: "))
+def main():
 
-# def info_aluno():
-#     print(f"O nome do aluno é {nome_aluno}")
-#     print(f"A matrícula do aluno é {matricula_aluno}")
+    print("--------- Analisador de Notas ---------")
+    print(" ")
 
-# info_aluno()
+    lista_alunos = []
 
-lista_alunos = []
+    while True:
+        nome = input("Informe o nome do aluno (ou 'sair' para encerrar): ")
+
+        if nome.lower() == 'sair':
+            break
+
+        matricula = int(input("Informe a matrícula do aluno: "))
+        n1 = float(input("Informe a primeira nota: "))
+        n2 = float(input("Informe a segunda nota: "))
+
+        media = (n1 + n2) / 2
+
+        aluno = {
+            "nome": nome,
+            "matricula": matricula,
+            "n1": n1,
+            "n2": n2,
+            "media" : media
+        }
+
+        lista_alunos.append(aluno)
+        print("Aluno cadastrado com sucesso!")
+        final = input("Deseja cadastrar outro aluno?: ")
+        if final.lower() != "n":
+            print("Cadastro de alunos finalizado!")
+            break
+
+    print("--------- Relatório de Notas ---------")
+    print(" ")
+
+    for aluno in lista_alunos:   
+        status = "Aprovado" if aluno["media"] >= 7 else "Reprovado"
+        print(f"Aluno: {aluno['nome']} | {aluno['matricula']} | {aluno['n1']} | {aluno['n2']} | Média: {aluno['media']:.2f} | Status: {status}")
 
 while True:
-    nome = input("Informe o nome do aluno (ou 'sair' para encerrar): ")
-    if nome.lower() == 'sair':
-        break
-    matricula = int(input("Informe a matrícula do aluno: "))
-    n1 = float(input("Informe a primeira nota: "))
-    n2 = float(input("Informe a segunda nota: "))
+    main()
 
-    aluno = {
-        "nome": nome,
-        "matricula": matricula,
-        "n1": n1,
-        "n2": n2
-    }
-    lista_alunos.append(aluno)
-    print("Aluno cadastrado com sucesso!")
+    while True:
+        resposta = input("Reiniciar programa?: ").lower().strip()
 
-print("--------- Relatório de Notas ---------")
-print(" ")
+        if resposta in ["s", "sim"]:
+            print("Reiniciando o programa...")
+            break 
 
-for aluno in lista_alunos:
-    print(f"Aluno: {aluno['nome']} | {aluno['matricula']} | {aluno['n1']} | {aluno['n2']}")
+        elif resposta in ["n", "nao", "não"]:
+            print("Finalizando o programa. Até mais!")
+            exit()
+
+        else:
+            print("Resposta inválida.")
